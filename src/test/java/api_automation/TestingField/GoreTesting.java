d59800698d66971df52473ee2c28df1e0dbf23d8
Chau Thai <chauthai2003@gmail.com>
2025-08-23 16:30:56 -0400

    jj

--- src/test/java/api_automation/TestingField/GoreTesting.java
+++ src/test/java/api_automation/TestingField/GoreTesting.java
@@ -7,7 +7,7 @@ import api_automation.requestBuilder.GoreRequestBuilder;
 import api_automation.utils.Log;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jcraft.jsch.Logger;
+
 import io.restassured.http.ContentType;
 
 import api_automation.utils.TestBase;
@@ -19,28 +19,32 @@ import org.testng.annotations.Test;
 
 public class GoreTesting extends TestBase {
 
-    @Test(dataProvider = "dataProvider",dataProviderClass = GorestDataProviderClass.class)
+    @Test(dataProvider = "dataProvider", dataProviderClass = GorestDataProviderClass.class)
     public void user_create_requestData(String email, String name, String gender, String status) throws JsonProcessingException {
-       Log.info("This is request builder");
-        GoreRequestBuilder builder = new GoreRequestBuilder();
-        Log.warn("this is setEmailhh");
-        builder.setEmail(email);
-        builder.setName(name);
-        Log.trace("this is setEmailhh");
-        builder.setGender(gender);
-        builder.setStatus(status);
-        ObjectMapper mapper=new ObjectMapper();
-        String mappBuilder=mapper.writerWithDefaultPrettyPrinter().writeValueAsString(builder);
-        Log.info("This is the beginning of post request");
-        Response response= given()
-                .header("Authorization","Bearer d923586bca288aef6e650810925a5426ad3a62d413a6501fe5cc0d97185d610c")
-                .contentType(ContentType.JSON)
-                .body(mappBuilder)
-                .when()
-                .post("https://gorest.co.in/public/v1/users");
-       int statusCode= response.getStatusCode();
+        for (int i = 0; i < 10; i++) {
 
 
+            Log.info("This is request builder");
+            GoreRequestBuilder builder = new GoreRequestBuilder();
+            Log.warn("this is setEmailhh");
+            builder.setEmail(email);
+            builder.setName(name);
+            Log.trace("this is setEmailhh");
+            builder.setGender(gender);
+            builder.setStatus(status);
+            ObjectMapper mapper = new ObjectMapper();
+            String mappBuilder = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(builder);
+            Log.info("This is the beginning of post request");
+            Response response = given()
+                    .header("Authorization", "Bearer d923586bca288aef6e650810925a5426ad3a62d413a6501fe5cc0d97185d610c")
+                    .contentType(ContentType.JSON)
+                    .body(mappBuilder)
+                    .when()
+                    .post("https://gorest.co.in/public/v1/users");
+            int statusCode = response.getStatusCode();
+
+
+        }
     }
 }
 
