613f56ae52d1e159efe9e5693710d34955146bf7
toan1231 <100801887+toan1231@users.noreply.github.com>
2025-08-23 17:37:39 -0400

    Update GoreTesting.java

--- src/test/java/api_automation/TestingField/GoreTesting.java
+++ src/test/java/api_automation/TestingField/GoreTesting.java
@@ -36,7 +36,7 @@ public class GoreTesting extends TestBase {
             String mappBuilder = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(builder);
             Log.info("This is the beginning of post request");
             Response response = given()
-                    .header("Authorization", "Bearer chauthaid923586bca288aef6e650810925a5426ad3a62d413a6501fe5cc0d97185d610c")
+                    .header("Authorization", "Bearer d923586bca288aef6e650810925a5426ad3a62d413a6501fe5cc0d97185d610c")
                     .contentType(ContentType.JSON)
                     .body(mappBuilder)
                     .when()
