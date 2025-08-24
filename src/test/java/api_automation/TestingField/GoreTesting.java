488a2b182410991bb037cbcea72c4950aaa52b4c
toan1231 <100801887+toan1231@users.noreply.github.com>
2025-08-24 10:48:35 -0400

    Update GoreTesting.java

--- src/test/java/api_automation/TestingField/GoreTesting.java
+++ src/test/java/api_automation/TestingField/GoreTesting.java
@@ -1,57 +1,57 @@
-package api_automation.TestingField;
-import static io.restassured.RestAssured.given;
-import api_automation.dataProviderClass.GorestDataProviderClass;
-import api_automation.requestBuilder.GoreRequestBuilder;
+// package api_automation.TestingField;
+// import static io.restassured.RestAssured.given;
+// import api_automation.dataProviderClass.GorestDataProviderClass;
+// import api_automation.requestBuilder.GoreRequestBuilder;
 
-//import api_automation.utils.Log;
-import api_automation.utils.Log;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+// //import api_automation.utils.Log;
+// import api_automation.utils.Log;
+// import com.fasterxml.jackson.core.JsonProcessingException;
+// import com.fasterxml.jackson.databind.ObjectMapper;
 
-import io.restassured.http.ContentType;
+// import io.restassured.http.ContentType;
 
-import api_automation.utils.TestBase;
+// import api_automation.utils.TestBase;
 
-import io.restassured.response.Response;
-
-
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
-public class GoreTesting extends TestBase {
-
-    @Test(dataProvider = "dataProvider", dataProviderClass = GorestDataProviderClass.class)
-    public void user_create_requestData(String email, String name, String gender, String status) throws JsonProcessingException, InterruptedException {
-        for (int i = 0; i < 3; i++) {
+// import io.restassured.response.Response;
 
 
-            Log.info("This is request builder");
-            GoreRequestBuilder builder = new GoreRequestBuilder();
-            Log.warn("this is setEmailhh");
-            builder.setEmail(email);
-            builder.setName(name);
-            Log.trace("this is setEmailhh");
-            builder.setGender(gender);
-            builder.setStatus(status);
-            ObjectMapper mapper = new ObjectMapper();
-            String mappBuilder = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(builder);
-            Log.info("This is the beginning of post request");
-            Response response = given()
-                  //  .auth()
-                  //  .preemptive().basic("admin","1117f3a4e4f5794833bfc2a1186a99e24b")
-                   .header("Authorization", "Bearer d923586bca288aef6e650810925a5426ad3a62d413a6501fe5cc0d97185d610c")
-                    .contentType(ContentType.JSON)
-                    .when()
-                   // .get("http://localhost:8080/job/testing2/build?token=chau123456789");
-             .get("https://gorest.co.in/public/v1/users?gender=male&status=inactive&id=4124");
-            //  http://localhost:8080/job/testing2/build?token=chau123456789
-            //https://gorest.co.in/public/v1/users?gender=male&status=inactive&id=4124
-            int statusCode = response.getStatusCode();
-        //    Assert.assertEquals(statusCode, 200);
-         //   System.out.println(response.prettyPrint()+"1111111111111111111");
-            Thread.sleep(1000);
+// import org.testng.Assert;
+// import org.testng.annotations.Test;
+
+// public class GoreTesting extends TestBase {
+
+//     @Test(dataProvider = "dataProvider", dataProviderClass = GorestDataProviderClass.class)
+//     public void user_create_requestData(String email, String name, String gender, String status) throws JsonProcessingException, InterruptedException {
+//         for (int i = 0; i < 3; i++) {
 
 
-        }
-    }
-}
+//             Log.info("This is request builder");
+//             GoreRequestBuilder builder = new GoreRequestBuilder();
+//             Log.warn("this is setEmailhh");
+//             builder.setEmail(email);
+//             builder.setName(name);
+//             Log.trace("this is setEmailhh");
+//             builder.setGender(gender);
+//             builder.setStatus(status);
+//             ObjectMapper mapper = new ObjectMapper();
+//             String mappBuilder = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(builder);
+//             Log.info("This is the beginning of post request");
+//             Response response = given()
+//                   //  .auth()
+//                   //  .preemptive().basic("admin","1117f3a4e4f5794833bfc2a1186a99e24b")
+//                    .header("Authorization", "Bearer d923586bca288aef6e650810925a5426ad3a62d413a6501fe5cc0d97185d610c")
+//                     .contentType(ContentType.JSON)
+//                     .when()
+//                    // .get("http://localhost:8080/job/testing2/build?token=chau123456789");
+//              .get("https://gorest.co.in/public/v1/users?gender=male&status=inactive&id=4124");
+//             //  http://localhost:8080/job/testing2/build?token=chau123456789
+//             //https://gorest.co.in/public/v1/users?gender=male&status=inactive&id=4124
+//             int statusCode = response.getStatusCode();
+//         //    Assert.assertEquals(statusCode, 200);
+//          //   System.out.println(response.prettyPrint()+"1111111111111111111");
+//             Thread.sleep(1000);
+
+
+//         }
+//     }
+// }
