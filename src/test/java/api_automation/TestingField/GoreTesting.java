356fde22d19b5cf22f55d2bbf66d81c1941a1ac3
toan1231 <100801887+toan1231@users.noreply.github.com>
2025-08-23 23:46:07 -0400

    Update GoreTesting.java

--- src/test/java/api_automation/TestingField/GoreTesting.java
+++ src/test/java/api_automation/TestingField/GoreTesting.java
@@ -40,7 +40,7 @@ public class GoreTesting extends TestBase {
                     .header("Authorization", "Bearer d923586bca288aef6e650810925a5426ad3a62d413a6501fe5cc0d97185d610c")
                     .contentType(ContentType.JSON)
                     .when()
-                    .get("https://gorest.co.in/public/v1/users?gender=male&status=inactive&id=4124");
+                    .get("http://localhost:8080/job/testing2/build?token=chau123456789");
             int statusCode = response.getStatusCode();
             Assert.assertEquals(statusCode, 200);
             response.prettyPrint();
