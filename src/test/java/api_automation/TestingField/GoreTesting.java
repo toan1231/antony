dd5ee3a0c09f588c319e19fcaf8b3fa3cf78c9d2
Chau Thai <chauthai2003@gmail.com>
2025-08-24 09:27:24 -0400

    ll

--- src/test/java/api_automation/TestingField/GoreTesting.java
+++ src/test/java/api_automation/TestingField/GoreTesting.java
@@ -37,17 +37,17 @@ public class GoreTesting extends TestBase {
             String mappBuilder = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(builder);
             Log.info("This is the beginning of post request");
             Response response = given()
-                    .auth()
-                    .preemptive().basic("admin","1117f3a4e4f5794833bfc2a1186a99e24b")
-                    .header("Authorization", "Bearer d923586bca288aef6e650810925a5426ad3a62d413a6501fe5cc0d97185d610c")
+//                    .auth()
+//                    .preemptive().basic("admin","1117f3a4e4f5794833bfc2a1186a99e24b")
+                   .header("Authorization", "Bearer d923586bca288aef6e650810925a5426ad3a62d413a6501fe5cc0d97185d610c")
                     .contentType(ContentType.JSON)
                     .when()
-                  .get("http://localhost:8080/job/testing2/build?token=chau123456789");
-             // .get("https://gorest.co.in/public/v1/users?gender=male&status=inactive&id=4124");
-          //  http://localhost:8080/job/testing2/build?token=chau123456789
+                  //  .get("http://localhost:8080/job/testing2/build?token=chau123456789");
+             .get("https://gorest.co.in/public/v1/users?gender=male&status=inactive&id=4124");
+            //  http://localhost:8080/job/testing2/build?token=chau123456789
             //https://gorest.co.in/public/v1/users?gender=male&status=inactive&id=4124
             int statusCode = response.getStatusCode();
-            Assert.assertEquals(statusCode, 201);
+            Assert.assertEquals(statusCode, 200);
             System.out.println(response.prettyPrint()+"1111111111111111111");
             Thread.sleep(1000);
 
@@ -55,4 +55,3 @@ public class GoreTesting extends TestBase {
         }
     }
 }
-
