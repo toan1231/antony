255b0bc782fd91b20b958ea889664a16b180f28a
toan1231 <100801887+toan1231@users.noreply.github.com>
2025-08-30 07:09:58 -0400

    Update GoreTesting.java

--- src/test/java/api_automation/TestingField/GoreTesting.java
+++ src/test/java/api_automation/TestingField/GoreTesting.java
@@ -20,33 +20,15 @@ import org.testng.annotations.Test;
 
 public class GoreTesting extends TestBase {
 
-    @Test(dataProvider = "dataProvider", dataProviderClass = GorestDataProviderClass.class)
+    @Test()
     public void user_create_requestData(String email, String name, String gender, String status) throws JsonProcessingException, InterruptedException {
         for (int i = 0; i < 3; i++) {
 
 
-            Log.info("This is request builder");
-            GoreRequestBuilder builder = new GoreRequestBuilder();
-            Log.warn("this is setEmailhh");
-            builder.setEmail(email);
-            builder.setName(name);
-            Log.trace("this is setEmailhh");
-            builder.setGender(gender);
-            builder.setStatus(status);
-            ObjectMapper mapper = new ObjectMapper();
-            String mappBuilder = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(builder);
-            Log.info("This is the beginning of post request");
-            Response response = given()
-                 //   .auth()
-                  //  .preemptive().basic("admin","1117f3a4e4f5794833bfc2a1186a99e24b")
-                   .header("Authorization", "Bearer d923586bca288aef6e650810925a5426ad3a62d413a6501fe5cc0d97185d610c")
-                    .contentType(ContentType.JSON)
-                    .when()
-                  //  .get("http://localhost:8080/job/testing2/build?token=chau123456789");
-                    .get("https://gorest.co.in/public/v1/users?gender=male&status=inactive&id=4124");
-            response.prettyPrint();
-           //   http://localhost:8080/job/testing2/build?token=chau123456789
-          //  https://gorest.co.in/public/v1/users?gender=male&status=inactive&id=4124
+        for (int i=0;i<7;i++){
+            System.out.println("4444333333333333");
+            Thread.sleep(3000);
+        }
 
 
 
